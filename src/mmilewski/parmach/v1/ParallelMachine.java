a0a6be79e27ff32f39bb5e34f8d4ccdad62d2339
Marcin Milewski <mmilewski@gmail.com>
2015-01-25 20:31:57 -0500

    First version of Parallel Machine for Java 8

---
+++ src/mmilewski/parmach/v1/ParallelMachine.java
@@ -0,0 +1,120 @@
+package mmilewski.parmach.v1;
+
+import com.google.common.collect.UnmodifiableIterator;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+import static com.google.common.collect.Iterators.partition;
+import static java.lang.Math.min;
+
+/**
+ * Consumes stream of items. Reads a batch of items, processes them in parallel, and calls client back
+ * with one results one-by-one.
+ *
+ * Processes 150MB file on 4 cores in 2 seconds, which is a good trade-off I believe.
+ *
+ * Requires Java 8
+ */
+public class ParallelMachine<ItemT, ResultT> {
+    public enum ActionOnError {
+        IGNORE, // ignore this one, and keep processing other tasks
+        ABORT_EVERYTHING // abort all tasks and return control to the client
+    }
+
+    private Function<ItemT, ResultT> itemProcessor;
+    private BiConsumer<ItemT, ResultT> onNextCallback;
+    private Function<Throwable, ActionOnError> onErrorCallback;
+
+    private int numOfParallelExecutors;
+    private int maxNumOfItemsInBatch;
+
+    private static final int HARD_LIMIT_FOR_NUM_OF_ITEMS_IN_BATCH = 500;
+
+    public ParallelMachine(int numOfParallelExecutors) {
+        this.numOfParallelExecutors = numOfParallelExecutors;
+        this.maxNumOfItemsInBatch = min(HARD_LIMIT_FOR_NUM_OF_ITEMS_IN_BATCH, numOfParallelExecutors * 5);
+    }
+
+    public ParallelMachine setProcessor(Function<ItemT, ResultT> itemProcessor) {
+        this.itemProcessor = itemProcessor;
+        return this;
+    }
+
+    public ParallelMachine setCallbacks(BiConsumer<ItemT, ResultT> onNextCallback,
+                                        Function<Throwable, ActionOnError> onErrorCallback) {
+        this.onNextCallback = onNextCallback;
+        this.onErrorCallback = onErrorCallback;
+        return this;
+    }
+
+    private class Task implements Callable<TaskResult> {
+        private ItemT item;
+
+        public Task(ItemT item) {
+            this.item = item;
+        }
+
+        @Override
+        public TaskResult call() throws Exception {
+            return new TaskResult(item, itemProcessor.apply(item));
+        }
+    }
+
+    private class TaskResult {
+        private final ItemT item;
+        private final ResultT result;
+
+        public TaskResult(ItemT item, ResultT result) {
+            this.item = item;
+            this.result = result;
+        }
+    }
+
+    public void processIterator(Iterator<ItemT> itemsIt) throws InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(numOfParallelExecutors);
+        try {
+            // For more info about ExecutorCompletionService see http://java.dzone.com/articles/executorcompletionservice
+            ExecutorCompletionService<TaskResult> executor = new ExecutorCompletionService<>(es);
+            UnmodifiableIterator<List<ItemT>> batchesIterator = partition(itemsIt, maxNumOfItemsInBatch);
+            boolean shouldAbort = false;
+            while (batchesIterator.hasNext() && !shouldAbort) {
+                List<ItemT> batchOfLines = batchesIterator.next();
+                shouldAbort = processBatch(es, executor, batchOfLines);
+            }
+        } finally {
+            es.shutdown();
+        }
+    }
+
+    private boolean processBatch(ExecutorService es, ExecutorCompletionService<TaskResult> executor, List<ItemT> batchOfLines) throws InterruptedException {
+        // submit all items in this batch to executor
+        batchOfLines.forEach(item -> executor.submit(new Task(item)));
+        // wait for all tasks to execute
+        for (ItemT onlyNumberOfItemsMattersSoYouShouldTakeAsManyItemsAsYouSubmitted : batchOfLines) {
+            Future<TaskResult> maybeResult = executor.take();
+            try {
+                TaskResult taskResult = maybeResult.get();
+                onNextCallback.accept(taskResult.item, taskResult.result);
+            } catch (ExecutionException e) {
+                ActionOnError actionOnError = onErrorCallback.apply(e);
+                switch (actionOnError) {
+                    case IGNORE:
+                        break;
+                    case ABORT_EVERYTHING:
+                        es.shutdownNow();
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/mmilewski/parmach/v1/ParallelMachineExample.java
@@ -0,0 +1,67 @@
+package mmilewski.parmach.v1;
+
+import com.google.common.collect.Iterators;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+import static java.lang.System.currentTimeMillis;
+
+public class ParallelMachineExample {
+    public static void main(String[] args) throws Exception {
+        new ParallelMachineExample().run();
+    }
+
+    public void run() throws Exception {
+        File inputFile = new File("c:/marcin/samplefile.txt");
+        int numOfParallelExecutors = 20;
+
+        // configure machine which will execute tasks in parallel.
+        ParallelMachine<ItemDescription, FetchedData> pm = new ParallelMachine<>(numOfParallelExecutors);
+        pm.setProcessor(item -> FetchedData.fetch(item.stringDescription));
+        pm.setCallbacks(
+            (item, result) -> {
+                println("item was processed and is ready for you to use. item: " + item.stringDescription.substring(0, 10));
+            },
+            exception -> {
+                println("error " + exception);
+                return ParallelMachine.ActionOnError.IGNORE;
+            }
+        );
+
+        // read file sequentially and process it in parallel
+        try (BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(inputFile)))) {
+            long startTimeMillis = currentTimeMillis();
+            {
+                Iterator<String> inputLinesIt = fileReader.lines().iterator();
+                Iterator<ItemDescription> itemDescriptionsIt = Iterators.transform(inputLinesIt, ItemDescription::new);
+                pm.processIterator(itemDescriptionsIt);
+            }
+            System.out.println("Processed all in " + (currentTimeMillis() - startTimeMillis) + " ms");
+        }
+    }
+
+    private static class ItemDescription {
+        private String stringDescription;
+
+        public ItemDescription(String stringDescription) {
+            this.stringDescription = stringDescription;
+        }
+    }
+
+    private static class FetchedData {
+        static FetchedData fetch(String dataToConstructRequest) {
+            // call a service... and return with results
+            return new FetchedData();
+        }
+    }
+
+    private void println(String s) {
+        // printing is slow
+//        System.out.println(s);
+    }
+}
+
